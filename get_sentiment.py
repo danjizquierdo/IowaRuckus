6f98360cc2fb8a52e2e0c4a60fe5c755b999ee2f
Daniel Izquierdo <dan.j.izquierdo@gmail.com>
2020-02-05 17:47:18 -0500

    Consolidate functions

--- get_sentiment.py
+++ get_sentiment.py
@@ -7,6 +7,16 @@ import datetime
 import re
 import logging
 from sys import argv
+import re
+import nltk
+from nltk import word_tokenize, FreqDist
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import string
+from wordcloud import WordCloud
+from PIL import Image
+import matplotlib.pyplot as plt
+from collections import Counter
 
 logging.basicConfig(filename='errors.log', filemode='a+', format='%(asctime)s: %(message)s', level=logging.ERROR)
 
@@ -18,6 +28,92 @@ api = tweepy.API(auth, wait_on_rate_limit=True)
 
 tweets = {}
 users = {}
+
+
+def process_tweet(tweet):
+    """ Takes in a string, returns a list words in the string that aren't stopwords
+    Parameters:
+        tweet (string):  string of text to be tokenized
+    Returns:
+        stopwords_removed (list): list of all words in tweet, not including stopwords
+    """
+    stopwords_list=stopwords.words('english') +list(string.punctuation)
+    stopwords_list += ["'",'"','...','``','…','’','‘','“',"''",'""','”','”','co',"'s'",'\'s','n\'t','\'m','\'re','amp','https']
+    tokens = nltk.word_tokenize(tweet)
+    stopwords_removed = [token.lower() for token in tokens if token not in stopwords_list]
+    return stopwords_removed
+
+
+def tokenized(series):
+    """ Takes in a series containing strings or lists of strings, and creates a single list of all the words
+    Parameters:
+        series (series): series of text in the form of strings or lists of string
+
+    Returns:
+        tokens (list): list of every word in the series, not including stopwords
+    """
+
+    corpus = ' '.join([tweet.lower() if type(tweet)==str else ' '.join([tag.lower() for tag in tweet]) for tweet in series])
+    tokens = process_tweet(corpus)
+    return tokens
+
+
+def wordfrequency(series, top):
+    """ Returns the frequency of words in a list of strings.
+    Parameters:
+        series (iterable): List of strings to be combined and analyzed
+        top (int): The number of top words to return.
+    Returns:
+        list (tuples): List of word and value pairs for the top words in the series.
+    """
+    frequencies = FreqDist(tokenized(series))
+    return frequencies.most_common(top)
+
+
+def create_wordcloud(series, tag, *top):
+    """ Take in a list of lists and create a WordCloud visualization for those terms.
+    Parameters:
+            series (iterable): A list of lists containing strings.
+    Returns:
+        None: The ouput is a visualization of the strings in series in terms of the
+            frequency of their occurrence.
+    """
+
+    vocab = tokenized(series)
+    if not top[0]:
+        top[0]=200
+    cloud=WordCloud(max_words=top[0]).generate(' '.join([word for word in vocab]))
+    plt.imshow(cloud,interpolation='bilinear')
+    plt.title(f'Most Common words for {tag}')
+    plt.plot(figsize = (48,24))
+    plt.axis('off')
+    plt.show();
+
+def strip_tweets(tweet):
+    '''Process tweet text to remove retweets, mentions,links and hashtags.'''
+    retweet = r'RT:? ?@\w+:?'
+    tweet = re.sub(retweet, '', tweet)
+    mention = r'@\w+'
+    tweet = re.sub(mention, '', tweet)
+    links = r'^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$'
+    tweet = re.sub(links, '', tweet)
+    tweet_links = r'https:\/\/t\.co\/\w+|http:\/\/t\.co\/\w+'
+    tweet = re.sub(tweet_links, '', tweet)
+    tweet_link = r'http\S+'
+    tweet = re.sub(tweet_link, '', tweet)
+    hashtag = r'#\w+'
+    hashtags = re.findall(hashtag, tweet)
+    tweet = re.sub(hashtag, '', tweet)
+    return tweet, hashtags
+
+def cluster_flocks(dicts):
+    hashtags = Counter()
+    tweets = []
+    for dic in dicts.values():
+        text, tag = strip_tweets(dic['text'])
+        hashtags.update(tag)
+        tweets.append([text, tag])
+    return tweets, hashtags
 
 
 def myconverter(o):
